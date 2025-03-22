abbaeadd7741be8e9a440f98c06c6feb662dc9ea
hatttri <57240541+hatttri@users.noreply.github.com>
2025-03-23 04:57:04 +0900

    Add GetSum unit test

---
+++ test/SampleApp.Tests.Unit/Controllers/CalculatorControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using SampleApp.Controllers;
+
+namespace SampleApp.Tests.Unit.Controllers;
+
+public class CalculatorControllerTests
+{
+    [Fact]
+    public void GetSum()
+    {
+        var controller = new CalculatorController();
+        Assert.Equal(2, controller.GetSum(1, 1));
+        Assert.Equal(5, controller.GetSum(2, 3));
+    }
+}
